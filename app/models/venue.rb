28f0f1649b0af6c1f5480fefb8cc6f336019bf42
francesca sandford <fran6san@gmail.com>
2023-02-08 10:15:54 +0000

    configuration for cloudinary and install active_storage

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,5 +1,6 @@
 class Venue < ApplicationRecord
   belongs_to :user
+  has_one_attached :photo
 
   validates :name, :location, presence: true
   validates :price_per_day, :size_of_band, presence: true, numericality: { only_integer: true }
