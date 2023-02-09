3e43528d96566ab93bdf26af94aaeeb70d802e31
Jon Dedman <jonathandedman@googlemail.com>
2023-02-09 21:00:38 +0000

    improve functionality of form for book a venue

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "venues#index"
   resources :venues do
-    resources :booking, only: [:new, :create]
+    resources :bookings, only: [:new, :create]
   end
-  resources :booking, only: [:show, :edit, :update] do
-    resources :review, only: [:create]
+  resources :bookings, only: [:index, :show, :edit, :update] do
+    resources :reviews, only: [:create]
   end
 end
