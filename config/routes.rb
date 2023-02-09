e025d5cbc230f19e68c9aba6da4f2eabce41b630
Jon Dedman <jonathandedman@googlemail.com>
2023-02-09 14:56:49 +0000

    add route to bookings new

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "venues#index"
-  resources :venues
+  resources :venues do
+    resources :bookings, only: [:new, :create]
+  end
 end
