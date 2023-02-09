0329ca06c5e0dada1a06caa8f9f5146b8264579e
francesca sandford <fran6san@gmail.com>
2023-02-09 12:23:54 +0000

    update booking model to change :confirmed default to false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_08_101405) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_09_114953) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,7 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_08_101405) do
   end
 
   create_table "bookings", force: :cascade do |t|
-    t.boolean "confirmed"
+    t.boolean "confirmed", default: false
     t.bigint "venue_id", null: false
     t.bigint "user_id", null: false
     t.date "start_date"
