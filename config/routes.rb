439cd534077708470ba28330d5923f156bc1da23
Ilaria Peroni <ilaria.peroni13@gmail.com>
2023-02-09 16:57:05 +0000

    change routes, create simple form for review inside of the venue show page and change create  action inside of reviews controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "venues#index"
-  resources :venues
+  resources :venues do
+    resources :booking, only: [:new, :create, :update] do
+      resources :review, only: [:new, :create]
+    end
+  end
 end
