0130b9b90408c69b6d0f559419c8b4b6abc8a3f9
Alessandro Agozzino <alexagozzino@gmail.com>
2023-02-09 18:26:13 +0000

    cloudinary update and show page fully working, style to finish

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "venues#index"
   resources :venues
+  # get "/venues/:id", to: "venues#new"
+  # post "/venues/:id", to:
 end
+
+
+# devise_for :users
+# root to: "venues#index"
+# resources :venues do
+# resources :booking, only: [:create, :update]
+# resources :review, only: [:create]
+# end
