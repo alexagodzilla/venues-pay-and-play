b472caa5ca2c46316a223b4be20b2ff3fbf2ce47
Alessandro Agozzino <alexagozzino@gmail.com>
2023-02-09 11:22:41 +0000

    create show page, show action with all the venues information, create navigation links between index page and show page through venues names

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,9 +1,18 @@
 class VenuesController < ApplicationController
+  before_action :set_venue, only: [:show]
   def index
     @venues = Venue.all
   end
 
   def show
+    @booking = Booking.new
+    @review = Review.new
+  end
+
+  private
+
+  def set_venue
+    @venue = Venue.find(params[:id])
   end
 end
 
