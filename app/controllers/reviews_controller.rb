6bb13194632e5dd5f48e81bbd0e0f4003aebe52c
francesca sandford <fran6san@gmail.com>
2023-02-11 10:34:48 +0000

    update reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,6 @@
 class ReviewsController < ApplicationController
+  before_action :set_review, only: %i[edit update destroy]
+
   def create
     @booking = Booking.find(params[:booking_id])
     @review = Review.new(review_params)
@@ -10,10 +12,33 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    @review.update(review_params)
+    if @review.save
+      redirect_to booking_path(@review.booking)
+      # unsure about the above line
+    else
+      redirect_to 'bookings/show', notice: "Review added."
+    end
+  end
+
+  def destroy
+    @review.delete
+    redirect_to booking_path(@review.booking), notice: "Review deleted."
+  end
+
   private
 
   def review_params
     params.require(:review).permit(:comment, :rating)
   end
 
+  def set_review
+    @review = Review.find(params[:id])
+  end
 end
+
+# index/show - we show reviews on bookings show
+# new - we create a new review on the booking show
