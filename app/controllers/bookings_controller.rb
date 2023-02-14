3c61f8c1ecf2dc1a4c7a67db26891b1315b45942
Jon Dedman <jonathandedman@googlemail.com>
2023-02-14 17:18:04 +0000

    add update and edit to controller, refactor booking form into partial

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -28,6 +28,16 @@ class BookingsController < ApplicationController
     end
   end
 
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    @booking.update(booking_params)
+    redirect_to booking_path(@booking)
+  end
+
   private
 
   def booking_params
