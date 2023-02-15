f51fc4ce055b7e881ecbeb2798271e14f7498998
Jon Dedman <jonathandedman@googlemail.com>
2023-02-15 22:19:26 +0000

    remove dependent destroy from child model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,5 @@
 class Review < ApplicationRecord
-  belongs_to :booking, dependent: :destroy # i have added dependent destroy here to delete the review when the booking is deleted.
-
+  belongs_to :booking
   validates :rating, presence: true, numericality: { only_integer: true }
   validates :comment, length: { minimum: 2 }
 end
