edfe401a3d83e7560b4c5c730b4277f07447cbc9
Jon Dedman <jonathandedman@googlemail.com>
2023-02-15 11:37:32 +0000

    update review model with dependant destroy to ensure a review is deleted if the booking is deleted

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
-  belongs_to :booking
+  belongs_to :booking, dependent: :destroy # i have added dependent destroy here to delete the review when the booking is deleted.
 
   validates :rating, presence: true, numericality: { only_integer: true }
   validates :comment, length: { minimum: 2 }
