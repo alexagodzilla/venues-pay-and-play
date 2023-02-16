a7181b8292b91f45fd28744c8d0374c4593e6b39
Jon Dedman <jonathandedman@googlemail.com>
2023-02-16 13:45:19 +0000

    update webpack config file to resolve yarn build errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,27 @@
-const path    = require("path")
-const webpack = require("webpack")
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-  mode: "production",
-  devtool: "source-map",
-  entry: {
-    application: "./app/javascript/application.js"
-  },
-  output: {
-    filename: "[name].js",
-    sourceMapFilename: "[file].map",
-    path: path.resolve(__dirname, "app/assets/builds"),
-  },
-  plugins: [
-    new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1
-    })
-  ]
-}
+	mode: "production",
+	devtool: "source-map",
+	entry: {
+		application: "./app/javascript/application.js",
+	},
+	output: {
+		filename: "[name].js",
+		sourceMapFilename: "[file].map",
+		path: path.resolve(__dirname, "app/assets/builds"),
+	},
+	plugins: [
+		new webpack.optimize.LimitChunkCountPlugin({
+			maxChunks: 1,
+		}),
+	],
+	resolve: {
+		extensions: [".js", ".jsx", ".json"],
+		alias: {
+			"@mapbox/mapbox-gl-geocoder$":
+				"@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.min.js",
+		},
+	},
+};
