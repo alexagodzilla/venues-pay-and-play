8ee2a668fb52b760ffab30cb7762e58132698886
francesca sandford <fran6san@gmail.com>
2023-07-08 14:45:41 +0100

    moved logic from view into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
 end
+
+def average_rating(venue_reviews)
+  all_ratings = 0
+  venue_reviews.each { |review| all_ratings += review.rating }
+  (all_ratings / venue_reviews.count.to_f).round(1)
+end
