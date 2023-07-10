14bf2c4b7c93244a404d3758256e69030de20801
francesca sandford <fran6san@gmail.com>
2023-07-10 17:49:25 +0100

    move helper method into module

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,7 @@
 module ApplicationHelper
-end
-
-def average_rating(venue_reviews)
-  all_ratings = 0
-  venue_reviews.each { |review| all_ratings += review.rating }
-  (all_ratings / venue_reviews.count.to_f).round(1)
+  def average_rating(venue_reviews)
+    all_ratings = 0
+    venue_reviews.each { |review| all_ratings += review.rating }
+    (all_ratings / venue_reviews.count.to_f).round(1)
+  end
 end
